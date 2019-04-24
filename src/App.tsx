546810d8d85478633739e9e207d669db1267e17b
Bruce Lefebvre <brucelefebvre@gmail.com>
2019-04-23 22:31:42 -0400

    Update to CRA react-scripts 3.0.0. Include redux-logger.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
-import { createStore, Store } from "redux";
+import { applyMiddleware, createStore, Store } from "redux";
+import logger from "redux-logger";
 import { rootReducer } from "./reducers/rootReducer";
-import logo from "./logo.svg";
-import "./App.css";
 import TimsRegister from "./containers/TimsRegister";
+import "./App.css";
 
 class App extends Component {
   private store: Store;
 
   constructor(props: any) {
     super(props);
-    this.store = createStore(rootReducer);
+    this.store = createStore(rootReducer, applyMiddleware(logger));
   }
 
   public render() {
--- src/components/TimsRegister.tsx
+++ src/components/TimsRegister.tsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 import { Numpad } from "./Numpad";
 import { FoodItems } from "./FoodItems";
 import { ExtraButtons } from "./ExtraButtons";
