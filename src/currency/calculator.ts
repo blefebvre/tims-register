94e733a0783b493110012505e76dd26c332fa2c0
Bruce Lefebvre <brucelefebvre@gmail.com>
2019-04-24 23:34:58 -0400

    Use big.js in the calculator
    Demo dry run

--- src/currency/calculator.ts
+++ src/currency/calculator.ts
@@ -1,15 +1,19 @@
 import { FoodItem } from "../models/FoodItem";
+import { Big } from "big.js";
 
 // Get the total value of all items
 export function getTotalCostOfItems(items: FoodItem[]): number {
-  // TODO: big.js
-  return items.reduce((previousValue: number, item: FoodItem) => {
-    if (item.price) {
-      return previousValue + item.price;
-    } else {
-      return previousValue;
-    }
-  }, 0);
+  return parseFloat(
+    items
+      .reduce((previousValue: Big, item: FoodItem) => {
+        if (item.price) {
+          return previousValue.plus(Big(item.price));
+        } else {
+          return previousValue;
+        }
+      }, Big(0))
+      .toString()
+  );
 }
 
 // Calculate the change due given the total and an amount of cash
@@ -17,7 +21,8 @@ export function getChangeDue(total: number, cash: number): string {
   if (cash <= 0) {
     return "-";
   }
+  const bigCash = Big(cash);
+  const bigTotal = Big(total);
 
-  // TODO: big.js
-  return (cash - total).toString();
+  return bigCash.minus(bigTotal).toFixed(2);
 }
